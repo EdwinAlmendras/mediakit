1e9e936ff807381c1e82b53916ca57cbea2ab412
Ealmfr <xxxedwin10xxx@gmail.com>
2025-12-20 12:09:39 +0100

    add mts extension

--- mediakit/core/extensions.py
+++ mediakit/core/extensions.py
@@ -3,7 +3,7 @@ Centralized media file extensions.
 
 Used by: mediakit, uploader, social, kmp
 """
-video_extensions = ['webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'rrc', 'gifv', 'mng', 'mov', 'avi', 'qt', 'wmv', 'yuv', 'rm', 'asf', 'amv', 'mp4', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'm4v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b', 'mod'] 
+video_extensions = ['webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'rrc', 'gifv', 'mts', 'mng', 'mov', 'avi', 'qt', 'wmv', 'yuv', 'rm', 'asf', 'amv', 'mp4', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'm4v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b', 'mod'] 
 VIDEO_EXTENSIONS = set(f".{extension}" for extension in video_extensions)
 
 IMAGE_EXTENSIONS = {
